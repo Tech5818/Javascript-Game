4686e038082adb880e7bcfe0798924e6f3191689
Tech5818 <pqow5818@gmail.com>
2024-09-02 13:28:43 +0900

    asdf

--- script/index.js
+++ script/index.js
@@ -1,11 +1,17 @@
 const rocket = document.getElementById("rocket");
 const scoreElement = document.getElementById("score"); // 점수를 표시할 HTML 요소
 const fuelElement = document.getElementById("fuel"); // 연료를 표시할 HTML 요소
+const gameOverScreen = document.getElementById("gameover");
+const recordElement = document.getElementById("record");
+const retryBtn = document.getElementById("retry_btn");
+const recordBtn = document.getElementById("record_btn");
+const gameOverMesageElement = document.getElementById("gameover_message");
+const topScoreElement = document.getElementById("top_score");
+const lastScoreEelement = document.getElementById("last_score");
 
 let xPos = 0; // x 위치
 let yPos = 0; // y 위치
-let getRocket; // requestAnimationFrame ID
-let randomRotateRocket;
+let getRocket; // rocket
 let rotate = 0; // 각도
 let direction; // 오른쪽으로 기울건지 왼쪽으로 기울건지
 const baseSpeed = 5; // 기본 속도 배수
@@ -17,6 +23,7 @@ let fuel = 100; // 초기 연료
 let isGameOver = false; // 게임 오버 상태
 const gravity = 0.5; // 중력 계수
 let velocity = 0; // 낙하 속도
+let gameOverMesage = "";
 
 // 랜덤으로 방향을 설정하는 함수
 const setDirection = setInterval(() => {
@@ -33,29 +40,26 @@ const setDirection = setInterval(() => {
 
 // 점수를 업데이트하는 함수
 const updateScore = () => {
-  // 각도의 절대값이 작을수록 score가 커지도록 설정
   const normalizedRotate = Math.abs(rotate) / 90;
-  score += Math.max(0.1, 5 * (1 - normalizedRotate)); // 점수를 계산 (최대 1000점, 절대값이 0에 가까울수록 점수 상승)
-  scoreElement.textContent = `Score: ${Math.round(score)}`; // 점수를 화면에 표시
+  score += Math.max(0.1, 5 * (1 - normalizedRotate));
+  scoreElement.textContent = `SCORE: ${Math.round(score)}`;
 };
 
 // 연료를 업데이트하는 함수
 const updateFuel = () => {
-  // 연료 감소
-  fuel -= 0.1; // 연료가 서서히 줄어드는 속도
-  fuel = Math.max(0, fuel); // 연료가 0보다 작아지지 않도록 제한
-  fuelElement.style.width = `${Math.round(fuel)}%`; // 연료를 화면에 표시
+  fuel -= 0.1;
+  fuel = Math.max(0, fuel);
+  fuelElement.style.width = `${Math.round(fuel)}%`;
 
-  // 연료가 0이 되면 게임 오버
   if (fuel <= 0) {
     if (!isGameOver) {
       isGameOver = true;
+      gameOverMesage = "Out of fuel";
       gameOver();
     }
   }
 };
 
-// 로켓 이동 및 애니메이션
 const fireRocket = () => {
   if (isGameOver) {
     return;
@@ -65,6 +69,10 @@ const fireRocket = () => {
   const normalizedRotate = Math.abs(rotate) / 90; // 각도를 0에서 1 사이로 정규화
   const speed = baseSpeed * Math.pow(normalizedRotate, speedFactor); // speedFactor로 속도 증가 계수 조정
   xPos += (rotate > 0 ? 1 : -1) * speed; // 각도에 따라 왼쪽이나 오른쪽으로 이동
+
+  if (rotate === 90 && rotate === -90) {
+    xPos += 15;
+  }
 
   // 각도에 따라 y 위치 조정
   yPos = 200 * (1 - normalizedRotate); // -100px에서 0px까지의 범위 조정
@@ -76,8 +84,6 @@ const fireRocket = () => {
 
   updateScore(); // 점수 업데이트
   updateFuel(); // 연료 업데이트
-
-  console.log(yPos);
 
   // 로켓이 body 경계를 벗어나면 게임 오버
   const rocketRect = rocket.getBoundingClientRect(); // 로켓의 위치와 크기 정보
@@ -91,6 +97,7 @@ const fireRocket = () => {
   ) {
     if (!isGameOver) {
       isGameOver = true;
+      gameOverMesage = "It's off track";
       gameOver();
     }
   }
@@ -98,9 +105,19 @@ const fireRocket = () => {
 
 // 게임 오버 시 실행되는 함수
 const gameOver = () => {
+  gameOverScreen.style.display = "flex";
+  recordElement.textContent = `record: ${Math.round(score)}`;
+  gameOverMesageElement.textContent = gameOverMesage;
   // 애니메이션을 멈추기
   cancelAnimationFrame(getRocket);
 
+  localStorage.setItem("last_score", score);
+
+  const topRecord = localStorage.getItem("top_score");
+
+  if (topRecord < score) {
+    localStorage.setItem("top_score", score);
+  }
   // 로켓 떨어지는 애니메이션 시작
   const dropRocket = () => {
     if (yPos >= window.innerHeight) {
@@ -130,7 +147,7 @@ const randomRotate = () => {
   if (rotate > 90) rotate = 90;
   if (rotate < -90) rotate = -90;
 
-  randomRotateRocket = requestAnimationFrame(randomRotate);
+  requestAnimationFrame(randomRotate);
 };
 
 // 방향키로 조정
@@ -148,7 +165,36 @@ const controlRocket = () => {
   });
 };
 
-// 애니메이션 시작
-fireRocket();
-randomRotate();
-controlRocket();
+retryBtn.addEventListener("click", () => {
+  location.reload();
+});
+
+const startScreen = document.getElementById("start");
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    startScreen.style.opacity = 0;
+
+    setTimeout(() => {
+      startScreen.style.display = "none";
+    }, 1000);
+    fireRocket();
+    randomRotate();
+    controlRocket();
+  }
+});
+
+let lastScore = localStorage.getItem("last_score");
+let topScore = localStorage.getItem("top_score");
+
+console.log(lastScore);
+
+if (!lastScore) {
+  lastScore = 0;
+}
+
+if (!topScore) {
+  topScore = 0;
+}
+lastScoreEelement.textContent = `last score: ${Math.round(lastScore)}`;
+topScoreElement.textContent = `top score: ${Math.round(topScore)}`;
